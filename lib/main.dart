437d18f5f3d1f02ef48fcf0ef4d2e25a6f74dc83
audy123ari <audy123ari@gmail.com>
2024-05-26 02:42:41 +0000

    Aplikasi Counter

--- lib/main.dart
+++ lib/main.dart
@@ -4,37 +4,52 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _count = 0;
+  double _fontSize = 48.0;
+
+  void _onPressed() {
+    setState(() {
+      _count++;
+      _fontSize += 4.0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Belajar Stateful',
       home: Scaffold(
-          appBar: AppBar(title: const Text('Testing Widget Text')),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(16.0),
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: const Text(
-                    'Hello World!!!!',
-                    maxLines: 3,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14.0,
-                    ),
-                  ),
+        appBar: AppBar(
+          title: const Center(child: Text('Belajar Stateful')),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$_count',
+                style: TextStyle(
+                  fontSize: _fontSize,
+                  fontWeight: FontWeight.bold,
                 ),
-              ],
-            ),
-          )),
+              ),
+              const SizedBox(height: 32.0),
+              ElevatedButton(
+                onPressed: _onPressed,
+                child: const Text('Tambah Bilangan'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
